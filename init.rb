e09bc471b1fe89bf5dc52695a7258a15820f0493
mauro torres <maurotorres@gmail.com>
2010-08-12 10:56:34 -0300

    some changes

--- init.rb
+++ init.rb
@@ -1,10 +1,28 @@
+=begin
+  Copyright (c) 2010 Kyle J. Ginavan & Mauro Torres.
+
+  This file is part of Heroku Jammit Plugin.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+=end
+
 begin
   require 'jammit'
 rescue LoadError
   raise "jammit gem is missing.  Please install jammit: sudo gem install jammit"
 end
-require File.dirname(__FILE__) + '/lib/heroku_jammit_command'
-require File.dirname(__FILE__) + '/lib/help'
 require 'yaml'
-require 'heroku'
+require File.dirname(__FILE__) + '/lib/heroku_jammit'
+require File.dirname(__FILE__) + '/lib/help'
 
